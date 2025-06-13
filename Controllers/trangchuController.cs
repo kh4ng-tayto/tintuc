57584ce29343f2e337f72258f40edb17207ff016
khang <khang@DESKTOP-CLGOSLF>
2025-06-13 10:54:55 +0700

    Add project files.

---
+++ Controllers/trangchuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vnexpress.Controllers
+{
+    public class trangchuController : Controller
+    {
+        // GET: trangchu
+        public ActionResult trangchu()
+        {
+            return View();
+        }
+    }
+}
