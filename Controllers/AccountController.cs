4864899fd260796d93bf39baf87344aee647386c
khang <khang@DESKTOP-CLGOSLF>
2025-07-19 16:23:56 +0700

    .

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vnexpress.Models;
+
+namespace Vnexpress.Controllers
+{
+    public class AccountController : Controller
+    {
+        QLtintucEntities2 db = new QLtintucEntities2();
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(user);
+        }
+
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Username == username && u.PasswordHash == password);
+            if (user != null)
+            {
+                // Lưu vào Session nếu bạn vẫn muốn
+                Session["UserId"] = user.Id;
+                Session["Username"] = user.Username;
+
+                // Tạo Cookie lưu tên đăng nhập (có thể thêm ID nếu muốn)
+                HttpCookie cookie = new HttpCookie("Username", user.Username);
+                cookie.Expires = DateTime.Now.AddDays(7); // tồn tại trong 7 ngày
+                Response.Cookies.Add(cookie);
+
+                return RedirectToAction("Index", "Articles");
+            }
+
+            ViewBag.Error = "Sai tài khoản hoặc mật khẩu";
+            return View();
+        }
+
+
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+
+            if (Request.Cookies["Username"] != null)
+            {
+                var cookie = new HttpCookie("Username");
+                cookie.Expires = DateTime.Now.AddDays(-1); // hết hạn
+                Response.Cookies.Add(cookie);
+            }
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vnexpress.Models;
+
+namespace Vnexpress.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Articles
+        QLtintucEntities2 db = new QLtintucEntities2();
+        public ActionResult Index()
+        {
+            return View(db.Articles.ToList());
+        }
+        public ActionResult Create()
+        {
+            var categories = db.Categories.Select(c => new { c.Id, c.Name }).ToList();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name");
+
+            var users = db.Users.Select(u => new { u.Id, u.FullName }).ToList();
+            ViewBag.UserId = new SelectList(users, "Id", "FullName");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Article article)
+        {
+            var culture = new System.Globalization.CultureInfo("vi-VN");
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+
+            var categories = db.Categories.Select(c => new { c.Id, c.Name }).ToList();
+            var users = db.Users.Select(u => new { u.Id, u.FullName }).ToList();
+
+            if (ModelState.IsValid)
+            {
+                article.CreatedDate = DateTime.Now.Date;
+                article.CreatedTime = DateTime.Now.TimeOfDay;
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", article.CategoryId);
+            ViewBag.UserId = new SelectList(users, "Id", "FullName", article.UserId);
+            return View(article);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Articles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            var categories = db.Categories.Select(c => new { c.Id, c.Name }).ToList();
+            var users = db.Users.Select(u => new { u.Id, u.FullName }).ToList();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", article.CategoryId);
+            ViewBag.UserId = new SelectList(users, "Id", "FullName", article.UserId);
+            return View(article);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Article article)
+        {
+            var categories = db.Categories.Select(c => new { c.Id, c.Name }).ToList();
+            var users = db.Users.Select(u => new { u.Id, u.FullName }).ToList();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingArticle = db.Articles.AsNoTracking().FirstOrDefault(a => a.Id == article.Id);
+                    if (existingArticle != null)
+                    {
+                        // Giữ nguyên ngày tạo ban đầu
+                        article.CreatedDate = existingArticle.CreatedDate;
+                        article.CreatedTime = existingArticle.CreatedTime;
+
+                        // Cập nhật ngày giờ chỉnh sửa
+                        article.UpdatedDate = DateTime.Now.Date;
+                        article.UpdatedTime = DateTime.Now.TimeOfDay;
+                    }
+
+                    db.Entry(article).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    foreach (var error in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in error.ValidationErrors)
+                        {
+                            ModelState.AddModelError("", $"Thuộc tính: {validationError.PropertyName}, Lỗi: {validationError.ErrorMessage}");
+                        }
+                    }
+                    ViewBag.CategoryId = new SelectList(categories, "Id", "Name", article.CategoryId);
+                    ViewBag.UserId = new SelectList(users, "Id", "FullName", article.UserId);
+                    return View(article);
+                }
+            }
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", article.CategoryId);
+            ViewBag.UserId = new SelectList(users, "Id", "FullName", article.UserId);
+            return View(article);
+        }
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
